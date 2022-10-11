30600d3dec3bcc1790ef8a43648b496c4f0d403c
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2022-10-10 22:59:38 -0500

    Listo para subir a heroku

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ import express from "express";
 import fileUpload from "express-fileupload";
 import postsRoutes from "./routes/posts.routes.js";
 
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //middlewares
 app.use(express.json());
@@ -10,11 +14,17 @@ app.use(express.json());
 app.use(
   fileUpload({
     useTempFiles: true,
-    tempFileDir: "./upload"
+    tempFileDir: "./upload",
   })
 );
 
 //routes
 app.use(postsRoutes);
+
+app.use(express.static(join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "../client/build/index.html"));
+})
 
 export default app;
